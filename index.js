cc910073576fbf2b46887717955d5055f66ca3a1
AgusMoran <agusnicolasmoran13@gmail.com>
2023-06-21 19:25:42 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const Miboton = document.getElementById('miBoton');
+function handleClick() {
+    alert('Que toca vo');
+}
+addEventListener("click", function(){alert("que toca wachin");});
+    
---
+++ script.js
@@ -0,0 +1,5 @@
+function ShowAlert() { alert('Hola, Seguime en ig @aguus_moran'); }
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
